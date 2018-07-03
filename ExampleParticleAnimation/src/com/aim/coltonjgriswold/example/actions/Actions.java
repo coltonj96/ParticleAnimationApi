a1483865b136c556063a5caf9b14484915ca752a
coltonj96 <coltonjgriswold@gmail.com>
2018-07-03 11:53:54 -0500

    Add files via upload

--- ExampleParticleAnimation/src/com/aim/coltonjgriswold/example/actions/Actions.java
+++ ExampleParticleAnimation/src/com/aim/coltonjgriswold/example/actions/Actions.java
@@ -10,25 +10,23 @@ public class Actions extends PAAction {
 
     public Actions(PAObject object) {
 	a = object;
-	b = new int[] { 0, 0, 0 };
+	b = new int[] { 0, 0 };
     }
 
     @Override
     public void run() {
-	if (b[0] < 8) {
-	    a.rotateX(10.0);
+	if (b[0] < 9) {
+	    a.rotateZ(-10.0);
+	    a.move(1.0, 0, 0);
 	    b[0] += 1;
 	}
-	if (b[0] == 9 && b[1] < 8) {
-	    a.rotateY(10.0);
+	if (b[0] == 9 && b[1] < 9) {
+	    a.rotateZ(10.0);
+	    a.move(-1.0, 0, 0);
 	    b[1] += 1;
 	}
-	if (b[1] == 9 && b[2] < 8) {
-	    a.rotateZ(10.0);
-	    b[2] += 1;
-	}
-	if (b[2] == 9)
-	    b = new int[] { 0, 0, 0 };
+	if (b[0] == 9 && b[1] == 9)
+	    b = new int[] { 0, 0 };
 	a.draw();
     }
 }
