e37c35bb3b6916da0f89f42fb48b51a826936141
coltonj96 <coltonjgriswold@aim.com>
2018-07-26 08:11:46 -0500

    Update

--- ExampleParticleAnimation/src/com/aim/coltonjgriswold/example/ExampleParticleAnimation.java
+++ ExampleParticleAnimation/src/com/aim/coltonjgriswold/example/ExampleParticleAnimation.java
@@ -1,6 +1,10 @@
 package com.aim.coltonjgriswold.example;
 
-import org.bukkit.Particle;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,33 +13,78 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
 
-import com.aim.coltonjgriswold.example.actions.Actions;
+import com.aim.coltonjgriswold.example.actions.Physics;
+import com.aim.coltonjgriswold.example.shapes.Cube;
 import com.aim.coltonjgriswold.paapi.api.graphics.PAAnimation;
-import com.aim.coltonjgriswold.paapi.api.graphics.geometry.PATris;
-import com.aim.coltonjgriswold.paapi.api.graphics.utilities.PAColor;
+import com.aim.coltonjgriswold.paapi.api.graphics.events.PAEntityInteractEvent;
+import com.aim.coltonjgriswold.paapi.api.graphics.events.PALivingEntityInteractEvent;
+import com.aim.coltonjgriswold.paapi.api.graphics.events.PAObjectMoveEvent;
+import com.aim.coltonjgriswold.paapi.api.graphics.events.PAPlayerClickEvent;
+import com.aim.coltonjgriswold.paapi.api.graphics.geometry.PAObject;
 
 public class ExampleParticleAnimation extends JavaPlugin implements Listener {
     
-    private boolean running;
-
+    private PAAnimation animation;
+    private Cube cube;
+    
     public void onEnable() {
-	running = false;
 	getServer().getPluginManager().registerEvents(this, this);
+	cube = new Cube(Bukkit.getWorlds().get(0).getSpawnLocation().add(2.0, 1.0, 0), 0.5);
+	cube.setAction(new Physics());
+	animation = new PAAnimation();
+	animation.addObject(cube);
+	animation.start(2L);
     }
     
     @EventHandler
     public void interact(PlayerInteractEvent event) {
-	if (event.getAction().equals(Action.LEFT_CLICK_AIR) && !running) {
-	    Player player = event.getPlayer();
-	    PATris tris1 = new PATris(Particle.REDSTONE, player.getEyeLocation().add(0, 1, 0), new Vector(0.75, 0.75, 0), new Vector(0, -0.75, 0), new Vector(-0.75, 0.75, 0), PAColor.fromRGB(255, 0, 0));
-	    PATris tris2 = new PATris(Particle.REDSTONE, player.getEyeLocation().add(0, 1, 0), new Vector(0.5, 0.5, 0), new Vector(0, -0.5, 0), new Vector(-0.5, 0.5, 0), PAColor.fromRGB(255, 0, 0));
-	    tris1.setAction(new Actions(event.getPlayer()));
-	    tris2.setAction(new Actions(event.getPlayer()));
-	    PAAnimation animation = new PAAnimation();
-	    animation.addObjects(tris1, tris2);
-	    animation.start(2);
-	    running = true;
+	Player player = event.getPlayer();
+	if (event.getAction().equals(Action.LEFT_CLICK_AIR) && !cube.isVisible()) {
+	    if (!player.hasPermission("paapi.spawn"))
+		return;
+	    cube.setVisible(true);
 	}
     }
-
+    
+    @EventHandler
+    public void interact(PAEntityInteractEvent event) {
+	Entity entity = event.getEntity();
+	PAObject object = event.getObject();
+	Location loc = object.getLocation();
+	Vector v = entity.getLocation().toVector().subtract(loc.toVector()).multiply(-1).normalize().multiply(object.getHitbox().divide(new Vector(2, 2, 2)));
+	object.move(v);
+	object.addVelocity(entity.getVelocity());
+    }
+    
+    @EventHandler
+    public void onMove(PAObjectMoveEvent event) {
+	Location loc = event.getObject().getLocation();
+	Block block = loc.getBlock();
+	if (block.isLiquid()) {
+	    switch (block.getType()) {
+	    case LAVA:
+		cube.setVisible(false);
+		event.getObject().setLocation(Bukkit.getWorlds().get(0).getSpawnLocation().add(2.0, 1.0, 0));
+		break;
+	    default:
+		break;
+	    }
+	}
+	if (loc.getY() <= 1.0) {
+	    cube.setVisible(false);
+	    event.getObject().setLocation(Bukkit.getWorlds().get(0).getSpawnLocation().add(2.0, 1.0, 0));
+	}
+    }
+    
+    @EventHandler
+    public void interact(PALivingEntityInteractEvent event) {
+	LivingEntity e = event.getLivingEntity();
+	e.damage(1.0);
+    }
+    
+    @EventHandler
+    public void onClick(PAPlayerClickEvent event) {
+	Player p = event.getPlayer();
+	Physics.setPlayer(p);
+    }
 }
--- ExampleParticleAnimation/src/com/aim/coltonjgriswold/example/shapes/Cube.java
+++ ExampleParticleAnimation/src/com/aim/coltonjgriswold/example/shapes/Cube.java
@@ -1,45 +1,55 @@
 package com.aim.coltonjgriswold.example.shapes;
 
+import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.util.Vector;
 
 import com.aim.coltonjgriswold.paapi.api.graphics.geometry.PAObject;
-import com.aim.coltonjgriswold.paapi.api.graphics.utilities.PAColor;
 import com.aim.coltonjgriswold.paapi.api.graphics.utilities.PANode;
 
 public class Cube extends PAObject {
 
-    public Cube(Particle type, Location location, PAColor color) {
-	this(type, location, color, 1.0);
+    public Cube(Location location) {
+	this(location, 1.0);
     }
 
-    public Cube(Particle type, Location location, PAColor color, double size) {
+    public Cube(Location location, double size) {
 	super(location);
 	PANode[] nodes = {
-		node(type, color, size, size, size),
-		node(type, color, -size, size, size),
-		node(type, color, -size, size, -size),
-		node(type, color, size, size, -size),
-		node(type, color, size, -size, size),
-		node(type, color, -size, -size, size),
-		node(type, color, -size, -size, -size),
-		node(type, color, size, -size, -size)
+		node(Particle.REDSTONE, Color.RED, size, size, size),
+		node(Particle.REDSTONE, Color.RED, -size, size, size),
+		node(Particle.REDSTONE, Color.RED, -size, size, -size),
+		node(Particle.REDSTONE, Color.RED, size, size, -size),
+		node(Particle.REDSTONE, Color.RED, size, -size, size),
+		node(Particle.REDSTONE, Color.RED, -size, -size, size),
+		node(Particle.REDSTONE, Color.RED, -size, -size, -size),
+		node(Particle.REDSTONE, Color.RED, size, -size, -size),
+		node(Particle.FLAME, Color.ORANGE, 0, 0, 0)
 	};
-	nodes[0].connect(nodes[1], nodes[4]);
-	nodes[1].connect(nodes[2], nodes[5]);
-	nodes[2].connect(nodes[3], nodes[6]);
-	nodes[3].connect(nodes[0], nodes[7]);
-	nodes[4].connect(nodes[5]);
-	nodes[5].connect(nodes[6]);
+	//nodes[0].connect(nodes[1], nodes[4]);
+	//nodes[1].connect(nodes[2], nodes[5]);
+	//nodes[2].connect(nodes[3], nodes[6]);
+	//nodes[3].connect(nodes[0], nodes[7]);
+	//nodes[4].connect(nodes[5]);
+	//nodes[5].connect(nodes[6]);
+	//nodes[6].connect(nodes[7]);
+	//nodes[7].connect(nodes[4]);
+	nodes[0].connect(nodes[1], nodes[2], nodes[4], nodes[5], nodes[7]);
+	nodes[1].connect(nodes[2], nodes[3], nodes[4], nodes[5], nodes[6]);
+	nodes[2].connect(nodes[3], nodes[5], nodes[6], nodes[7]);
+	nodes[3].connect(nodes[0], nodes[4], nodes[6], nodes[7]);
+	nodes[4].connect(nodes[5], nodes[6]);
+	nodes[5].connect(nodes[6], nodes[7]);
 	nodes[6].connect(nodes[7]);
 	nodes[7].connect(nodes[4]);
 	addNodes(nodes);
     }
 
-    private PANode node(Particle type, PAColor color, double x, double y, double z) {
+    private PANode node(Particle type, Color color, double x, double y, double z) {
 	PANode node = new PANode(type, new Vector(x, y, z));
-	node.setColor(color);
+	if (node.isColorable())
+	    node.setColor(color);
 	return node;
     }
 }
